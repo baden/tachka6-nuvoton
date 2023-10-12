6477c0381824d8c6c16495e8186fba4e4fb30c0e
Денис <baden.i.ua@gmail.com>
2023-10-13 01:03:15 +0300

    Working on receiver.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include "NUC100Series.h"
 #include "uart.h"
 #include "i2c.h"
@@ -19,6 +20,8 @@
 //     SYS->GPB_MFP &= ~(SYS_GPB_MFP_PB0_Msk | SYS_GPB_MFP_PB1_Msk);
 //     SYS->GPB_MFP |= (SYS_GPB_MFP_PB0_UART0_RXD | SYS_GPB_MFP_PB1_UART0_TXD);
 // }
+
+
 
 void SYS_Init(void)
 {
@@ -44,8 +47,9 @@ void SYS_Init(void)
     /* Set core clock as PLL_CLOCK from PLL */
     CLK_SetCoreClock(PLL_CLOCK);
 
-    /* Enable UART module clock */
+    /* Enable UART modules clock */
     CLK_EnableModuleClock(UART0_MODULE);
+    CLK_EnableModuleClock(UART1_MODULE);
 
   
     /* Enable I2C0 module clock */
@@ -53,31 +57,144 @@ void SYS_Init(void)
 
     /* Select UART module clock source */
     CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART_S_HXT, CLK_CLKDIV_UART(1));
+    CLK_SetModuleClock(UART1_MODULE, CLK_CLKSEL1_UART_S_HXT, CLK_CLKDIV_UART(1));
 
     /*---------------------------------------------------------------------------------------------------------*/
     /* Init I/O Multi-function                                                                                 */
     /*---------------------------------------------------------------------------------------------------------*/
 
-    /* Set GPB multi-function pins for UART0 RXD and TXD */
-    SYS->GPB_MFP &= ~(SYS_GPB_MFP_PB0_Msk | SYS_GPB_MFP_PB1_Msk);
-    SYS->GPB_MFP |= (SYS_GPB_MFP_PB0_UART0_RXD | SYS_GPB_MFP_PB1_UART0_TXD);
+    /* Set GPB multi-function pins for UART0 and UART1 RXD and TXD */
+    SYS->GPB_MFP &= ~(SYS_GPB_MFP_PB0_Msk | SYS_GPB_MFP_PB1_Msk |
+                    SYS_GPB_MFP_PB4_Msk | SYS_GPB_MFP_PB5_Msk);
+    SYS->GPB_MFP |= (SYS_GPB_MFP_PB0_UART0_RXD | SYS_GPB_MFP_PB1_UART0_TXD |
+                    SYS_GPB_MFP_PB4_UART1_RXD | SYS_GPB_MFP_PB5_UART1_TXD);
 
     /* Set GPA multi-function pins for I2C0 SDA and SCL */
     SYS->GPA_MFP &= ~(SYS_GPA_MFP_PA8_Msk | SYS_GPA_MFP_PA9_Msk);
     SYS->GPA_MFP |= SYS_GPA_MFP_PA8_I2C0_SDA | SYS_GPA_MFP_PA9_I2C0_SCL;
+}
 
+bool led_state = false;
+
+uint8_t bus_data[32] = {0};
+size_t bus_data_len = 0;
+
+volatile int bus_packet_counter = 0;
+
+enum bus_state {
+    BUS_GET_LENGTH,
+    BUS_GET_DATA,
+    BUS_GET_CRCL,
+    BUS_GET_CRCH,
+    BUS_DISCARD
+};
+
+enum bus_state bus_state = BUS_GET_LENGTH;
+
+void parseBusCommand(uint8_t cmd, uint8_t len, uint8_t *data, size_t data_len)
+{
+    printf("Bus command: %02x, len: %d\n", cmd, len);
+    for(int i = 0; i < data_len; i++) {
+        printf("%02x ", data[i]);
+    }
+    printf("\n");
+}
+
+// <len><cmd><data....><chkl><chkh>
+
+void parseBusPacket(uint8_t b)
+{
+    static uint8_t buffer[32] = {0};
+    static size_t len = 0;
+    static size_t ptr = 0;
+    static uint16_t chksum = 0;
+
+    switch(bus_state) {
+        case BUS_GET_LENGTH:
+            // Only max 0x20 is allowed
+            if(b <= 0x20) {
+                ptr = 0;
+                len = b - 3;
+                chksum = 0xFFFF - b;
+                bus_state = BUS_GET_DATA;
+            } else {
+                bus_state = BUS_DISCARD;
+            }
+            break;
+        case BUS_GET_DATA:
+            buffer[ptr++] = b;
+            chksum -= b;
+            if(ptr >= len) {
+                bus_state = BUS_GET_CRCL;
+            }
+            break;
+        case BUS_GET_CRCL:
+            if((chksum & 0xFF) == b) {
+                bus_state = BUS_GET_CRCH;
+            } else {
+                bus_state = BUS_DISCARD;
+            }
+            break;
+        case BUS_GET_CRCH:
+            if(((chksum >> 8) & 0xFF) == b) {
+                // Packet is valid
+                parseBusCommand(buffer[0], buffer[1], buffer + 2, len);
+                bus_state = BUS_GET_LENGTH;
+            } else {
+                bus_state = BUS_DISCARD;
+            }
+            break;
+        case BUS_DISCARD:
+                led_state = !led_state;
+                if(led_state) {
+                    PA10 = 1;
+                } else {
+                    PA10 = 0;
+                }
+            break;
+        default:
+            break;
+    }
+}
+
+void UART1_IRQHandler(void)
+{
+    volatile __attribute__((unused)) uint32_t u32IntSts = UART1->ISR;
+    volatile uint8_t u8InChar;
+
+    /* Rx Ready or Time-out INT */
+    if(UART_GET_INT_FLAG(UART1, UART_ISR_RDA_INT_Msk) || UART_GET_INT_FLAG(UART1, UART_ISR_TOUT_INT_Msk)) {
+        /* Handle received data */
+        u8InChar = UART_READ(UART1);
+
+        bus_packet_counter++;
+
+        parseBusPacket(u8InChar);
+
+        // if(bus_data_len < 32) {
+        //     bus_data[bus_data_len++] = UART_READ(UART1);
+        // } else {
+        //     // Read UART1 to clear interrupt flag
+        //     UART_READ(UART1);
+        // }
+
+    }
 }
 
 void UART0_Init()
 {
-    /*---------------------------------------------------------------------------------------------------------*/
-    /* Init UART                                                                                               */
-    /*---------------------------------------------------------------------------------------------------------*/
     /* Reset UART0 */
     SYS_ResetModule(UART0_RST);
-
     /* Configure UART0 and set UART0 Baudrate */
     UART_Open(UART0, 115200);
+}
+
+void UART1_Init()
+{
+    /* Reset UART1 */
+    SYS_ResetModule(UART1_RST);
+    /* Configure UART1 and set UART1 Baudrate */
+    UART_Open(UART1, 115200);
 }
 
 void I2C0_Init(void)
@@ -180,6 +297,11 @@ int main()
     /* Init UART0 for printf */
     UART0_Init();
 
+    /* Init UART1 for FS-IA6B receiver bus */
+    UART1_Init();
+    UART_ENABLE_INT(UART1, UART_IER_RDA_IEN_Msk);
+    NVIC_EnableIRQ(UART1_IRQn);
+
     /* Init I2C0 */
     I2C0_Init();
 
@@ -188,7 +310,7 @@ int main()
     printf("+------------------------+\n\n");
 
     GPIO_SetMode(PA, BIT10, GPIO_PMD_OUTPUT);
-    PA10 = 0;
+    PA10 = 1;
 
     // int8_t ch;
 
@@ -214,11 +336,11 @@ int main()
         // printf("Input: ");
         // ch = getchar();
         // printf("%c\n", ch);
-        PA10 = 1;
+        // PA10 = 1;
+        // Delay(1000000);
+        // PA10 = 0;
         Delay(1000000);
-        PA10 = 0;
-        Delay(1000000);
-        printf("Task tick. %d\n", counter);
+        // printf("Task tick. %d (bus: %d)\n", counter, bus_packet_counter);
         counter += 50;
         mcp4725_set_value(counter);
         if(counter >= 4096) counter = 0;
